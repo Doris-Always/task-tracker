2eccd245b6434aea3f81dbf304a2c4786e6c3615
Doris-Always <dorisebele47@gmail.com>
2021-08-14 21:29:44 -0700

    first commit

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,25 @@
+// import { TasksComponent } from './../tasks/tasks.component';
+import { Component, OnInit, Input } from '@angular/core';
+import { Task } from './../../Task';
+
+
+
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+
+  // @Input() task: Task
+
+  constructor() {
+
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,43 @@
+import {Task} from './Task'
+
+
+export const TASKS: Task [] = [
+{
+  id: 1,
+  text: 'Doctors Appointment',
+  day: 'July 25th at 12:00pm',
+  reminder: true,
+},
+{
+  id: 2,
+  text: 'Meeting with Digitanotion Developers',
+  day: 'July 30th at 1:30pm',
+  reminder: true, 
+},
+{
+  id: 3,
+  text: 'End of the month Report',
+  day: 'August 13th at 10:00pm',
+  reminder: false,
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
